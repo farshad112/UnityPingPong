d43da8a25b6b1bb97065ce809223d9e432e1ec04
Farshad Bin Fazle <farshad1563@gmail.com>
2016-07-07 01:09:50 +0600

    Add files via upload

---
+++ Assets/BallScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed = 30;
+    public Text BlueScore;
+    public Text GreenScore;
+
+    public int lastBlueScore = 0;
+    public int lastGreenScore = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //initial velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        BlueScore.text = lastBlueScore.ToString();
+        GreenScore.text = lastGreenScore.ToString();
+    }
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        // Hit the left Racket?
+        if (col.gameObject.name == "PongRacketLeft")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "PongRacketRight")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+
+        }
+
+        if (col.gameObject.name == "5inchPongWallHorizontalRight")
+        {            
+                lastBlueScore = lastBlueScore + 1;
+                BlueScore.text = lastBlueScore.ToString();                       
+        }
+        if (col.gameObject.name == "5inchPongWallHorizontalLeft")
+        {
+            lastGreenScore = lastGreenScore + 1;
+            GreenScore.text = lastGreenScore.ToString();
+        }
+    }
+}
---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour {
+    /*
+       We will also add a speed variable to our Script, so that we can control the racket's movement speed:
+    */
+    public float speed = 60;  //We made the speed variable public so that we can always modify it in the Inspector without changing the Script:
+    public string axis = "Vertical";
+    // Fixed Update method is called over and over again in a fixed time interval. Unity's physics are calculated at exact same time interval.
+    void FixedUpdate ()
+    {
+        /*
+        we use GetAxisRaw to check the vertical input axis. 
+        This will return 1 when pressing either the W key, the UpArrow key, or when pointing a gamepad's stick upwards.
+        It will return -1 when using the S key, the DownArrow key, or when pointing a gamepad's stick downwards.
+        It will return 0 when none of those keys are pressed.
+        */
+        float v = Input.GetAxisRaw(axis);
+        /*
+        we can use GetComponent to access the racket's Rigidbody component and then set its velocity:
+        */
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
